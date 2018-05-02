cf61b5fc99941b1f6c60cbf971b790c2b49e829e
usuarioso <usuarioso@debian>
2018-05-02 13:17:00 +0200

    Terminado server y cliente TCP

--- Practica2.2/Ejercicio5.c
+++ Practica2.2/Ejercicio5.c
@@ -13,8 +13,9 @@ int main(int argc, char** argv){
 
 	struct addrinfo hints;
 	memset((void*) &hints,'\0', sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_family = AF_INET;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_socktype = SOCK_STREAM;
 
 	struct addrinfo* res;
 	int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
@@ -23,26 +24,51 @@ int main(int argc, char** argv){
 		return -1;
 	}
 
-	char buffer[80];
-	int sd = socket(res->ai_family, res->ai_socktype, 0);
 	
-	int error = 0;
-	error = connect(sd,res->ai_addr, res->ai_addrlen);
-	std::cin >> buffer;
-	int i = 0;
-	do{
-		size_t siz = send(sd, buffer, i, 0);
-		i++;
-	} while(i < 80);
-	
-	int c = 0; i = 0;
-	do {
-	   	c = recv(sd, &(buffer[i]), 1, 0);
-	} while ( c >= 0 && i < 79 && buffer[i++] != '\n');
-	  		
-    
-	std::cout << buffer << "\n";
+	int sd = socket(res->ai_family, res->ai_socktype, 6);
+	if(sd == -1){
+		std::cout << "Error: " <<  gai_strerror(sd) << std::endl;
+		return -1;
+	}
 
+	int error = connect(sd, (struct sockaddr *) res->ai_addr, res->ai_addrlen);
+	if(error != 0){
+		std::cout << "Error: " << gai_strerror(error) << std::endl;
+		return -1;
+	}
+
+	
+	while(true){
+		char buffer[80];
+		int i = 0;
+		int c = 0;
+		std::cin >> buffer;
+
+		//std::cout << buffer;
+		size_t siz = send(sd, buffer, 80, 0);
+
+		if (buffer[0] == 'q' && buffer[1] == '\0') {
+			shutdown(sd, SHUT_RDWR);
+			c = 0; i = 0;
+			do {
+			   	c = recv(sd, &(buffer[i]), 1, 0);
+			   	i++;
+			} while ( c >= 0 && i < 79);
+			break;
+		}
+		else {
+			memset(buffer,0, 80);
+
+			c = 0; i = 0;
+			do {
+			   	c = recv(sd, &(buffer[i]), 1, 0);
+			   	i++;
+			} while ( c >= 0 && i < 79);
+			  		
+			std::cout << buffer << "\n";
+			memset(buffer,0, 80);
+		}
+	}
 	freeaddrinfo(res);
 	return 0;
 }
