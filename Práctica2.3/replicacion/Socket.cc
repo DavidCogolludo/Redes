c804d618afcc2e932da6b829df0a9f27b78c2edc
usuarioso <usuarioso@debian>
2018-05-18 15:38:57 +0200

    Arreglado

--- Práctica2.3/replicacion/Socket.cc
+++ Práctica2.3/replicacion/Socket.cc
@@ -11,6 +11,12 @@
 
 bool operator== (const Socket &s1, const Socket &s2)
 {
+    struct sockaddr_in * a, * b;
+    a = (struct sockaddr_in *)&s1.sa;
+    b = (struct sockaddr_in *)&s2.sa;
+    
+  return(a->sin_addr.s_addr == b->sin_addr.s_addr && a->sin_family == b->sin_family &&
+    a->sin_port == b->sin_port);
 }
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
@@ -34,8 +40,9 @@ Socket::Socket(const char * address, const char * port):sd(-1)
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
-	getaddrinfo(address, port, &hints, &sa);
-    sd = socket(sa->ai_family, sa->ai_socktype, 0);
+	struct addrinfo* res;
+	int rc = getaddrinfo(address, port, &hints, &res);
+    sd = socket(res->ai_family, res->ai_socktype, 0);
 
     listen(sd, 5);
 }
@@ -51,18 +58,23 @@ int Socket::bind()
 
 int Socket::send(Serializable * obj, Socket * sock)
 {
-    return send(sock, obj->data(), obj->size(), 0);
+    return sendto(sd, obj->data(), obj->size(), 0, (struct sockaddr *) &sa, sa_len);
 }
 
 // ----------------------------------------------------------------------------
 
 int Socket::recv(char * buffer, Socket ** sock)
 {
-    int c = 0; int i = 0;
+	char host[NI_MAXHOST];
+	char serv [NI_MAXSERV];
     
-        c = recvfrom(sock, &(buffer[i]), 1, 0, addr, addrlen);
+    int bytes = recvfrom(sd, buffer, 256, 0, (struct sockaddr *) &sa, &sa_len);
+    buffer[bytes]='\0';
+
+    getnameinfo((struct sockaddr *) &sa, sa_len, host, NI_MAXHOST,
+    	    	  serv, NI_MAXSERV, NI_NUMERICHOST|NI_NUMERICSERV);
     
-    *sock = new Socket(addr, addrlen)           
+    *sock = new Socket(sa, serv);
 }
 
 // ----------------------------------------------------------------------------
