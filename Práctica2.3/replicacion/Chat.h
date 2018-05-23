f0a8586ca283ea50fd1701df842f71b8948fcea6
DavidCogolludo <davipe05@ucm.es>
2018-05-23 09:55:26 +0200

    Hecho chat.h

--- Práctica2.3/replicacion/Chat.h
+++ Práctica2.3/replicacion/Chat.h
@@ -12,14 +12,32 @@ public:
 
     ChatMessage(const char * n, const std::string m)
     {
+    	strncpy(nick, n, 8);
+
+    	message = m;
     };
 
     void to_bin()
     {
+    	 int32_t total = 80 * sizeof(char) + 2 * sizeof(int16_t);
+    	 alloc_data(total);
+
+    	 char * tmp = _data + sizeof(int32_t);
+    	 memcpy(tmp, (void*)nick, 8);
+    	 tmp+= 8;
+    	 memcpy(tmp, (void*)message, 80);
+    	 tmp += 80;
     }
 
     virtual int from_bin(char * bobj)
     {
+    	char *tmp = data + sizeof(int32_t);
+    	memcpy(nick, tmp, 8);
+    	tmp+= 8;
+    	memcpy(message, tmp, 80);
+    	tmp += 80;
+
+    	return 0;
     }
 
     char message[80];
@@ -37,6 +55,12 @@ public:
 
     void do_message(char * buffer)
     {
+    	ChatMessage m;
+    	m.from_bin(buffer);
+
+    	for(int i = 0; i < connections.size(); i++){
+    		socket.send(&m, connections[i]);
+    	}
 
     }
 };
@@ -49,10 +73,31 @@ public:
 
     void input_thread()
     {
+    	char c[80];
+    	ChatMessage* m;
+    	while(1){
+
+    	    std::cin >> c;
+    	    m = new ChatMessage(nick, c);
+    	    m->to_bin();
+    	    socket.send(m, &socket);
+    	}
     }
 
     void net_thread()
     {
+    	char buf[80];
+    	ChatMessage m;
+
+    	while(1){
+
+    	    socket.recv(buf);
+    	    m.from_bin(buf);
+
+    	    if(m.nick != nick){
+    			std::cout << m.nick <<  ": "<< m.message;
+    	    }
+    	}
     }
 
 private:
