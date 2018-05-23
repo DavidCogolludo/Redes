b50e16080a36576f8c0f373a2878b316f3d0c2bd
DavidCogolludo <davipe05@ucm.es>
2018-05-23 09:31:10 +0200

    Creo que el socket está acabado

--- Práctica2.3/replicacion/Socket.cc
+++ Práctica2.3/replicacion/Socket.cc
@@ -17,6 +17,7 @@ bool operator== (const Socket &s1, const Socket &s2)
     
   return(a->sin_addr.s_addr == b->sin_addr.s_addr && a->sin_family == b->sin_family &&
     a->sin_port == b->sin_port);
+
 }
 
 std::ostream& operator<<(std::ostream& os, const Socket& s)
@@ -27,7 +28,7 @@ std::ostream& operator<<(std::ostream& os, const Socket& s)
     getnameinfo((struct sockaddr *) &(s.sa), s.sa_len, host, NI_MAXHOST, serv,
                 NI_MAXSERV, NI_NUMERICHOST);
 
-    os << host << ":" << serv;
+    return os << host << ":" << serv;
 }
 
 // ----------------------------------------------------------------------------
@@ -37,14 +38,18 @@ Socket::Socket(const char * address, const char * port):sd(-1)
 
 	struct addrinfo hints;
 	memset((void*) &hints,'\0', sizeof(struct addrinfo));
+
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_DGRAM;
 
 	struct addrinfo* res;
 	int rc = getaddrinfo(address, port, &hints, &res);
+	sa = res->ai_addr;
+	sa_len = res->ai_addrlen;
+
     sd = socket(res->ai_family, res->ai_socktype, 0);
 
-    listen(sd, 5);
+//    listen(sd, 5);
 }
 
 // ----------------------------------------------------------------------------
@@ -58,7 +63,9 @@ int Socket::bind()
 
 int Socket::send(Serializable * obj, Socket * sock)
 {
-    return sendto(sd, obj->data(), obj->size(), 0, (struct sockaddr *) &sa, sa_len);
+	obj->to_bin();
+
+    return sendto(sock->sd, (void*)obj->data(), obj->size(), 0, (struct sockaddr *) &sock->sa, sock->sa_len);
 }
 
 // ----------------------------------------------------------------------------
@@ -67,14 +74,20 @@ int Socket::recv(char * buffer, Socket ** sock)
 {
 	char host[NI_MAXHOST];
 	char serv [NI_MAXSERV];
-    
-    int bytes = recvfrom(sd, buffer, 256, 0, (struct sockaddr *) &sa, &sa_len);
-    buffer[bytes]='\0';
 
-    getnameinfo((struct sockaddr *) &sa, sa_len, host, NI_MAXHOST,
-    	    	  serv, NI_MAXSERV, NI_NUMERICHOST|NI_NUMERICSERV);
+	struct sockaddr cliente;
+	socklen_t cliente_len = sizeof(struct sockaddr);
+
+	buffer = (char*)malloc(MAX_MESSAGE_SIZE);
+	if(sock != 0){
+		int bytes =  recvfrom(sd, (void* )&buffer, MAX_MESSAGE_SIZE, 0, &cliente, &cliente_len);
+
+		*sock = new Socket(&cliente,cliente_len);
+	}else {
+		int bytes =  recvfrom(sd, (void* )&buffer, MAX_MESSAGE_SIZE, 0, &cliente, &cliente_len);
+	}
     
-    *sock = new Socket(sa, serv);
+    return 0;
 }
 
 // ----------------------------------------------------------------------------
